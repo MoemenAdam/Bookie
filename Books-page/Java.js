34dbe5723259967bd60d475a8dd7090d01a68b5e
Moemen Adam <112325070+MoemenAdam@users.noreply.github.com>
2022-11-20 12:58:43 +0200

    Add files via upload

---
+++ Books-page/Java.js
@@ -0,0 +1,13 @@
+var innerList = document.getElementById("innerList");
+
+// making the show menu function
+
+function showMenu() {
+    innerList.style.right = "0";
+}
+
+// making the hdie menu function 
+
+function hideMenu() {
+    innerList.style.right = "-200px";
+}
